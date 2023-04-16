f8ddd4206848eabd90a568c76759798ca63cf908
p4nthera115 <asdshuja@hotmail.co.uk>
2023-04-16 16:46:24 +0100

    Dynamic Routes

--- Next/nextjs-blog/pages/index.js
+++ Next/nextjs-blog/pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import Date from "../components/date";
 import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
@@ -30,11 +32,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
