f9d2c9c8f9a5af2037c41f29041bf1bcd1436320
p4nthera115 <asdshuja@hotmail.co.uk>
2023-05-05 15:28:57 +0100

    finished Login functionality

--- Next/airbnb-clone-tut/app/components/navbar/Navbar.tsx
+++ Next/airbnb-clone-tut/app/components/navbar/Navbar.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { User } from "@prisma/client";
 import { Container } from "../Container";
 import { Logo } from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
-export const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+export const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+  console.log(currentUser);
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div
@@ -27,7 +33,7 @@ export const Navbar = () => {
           >
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
