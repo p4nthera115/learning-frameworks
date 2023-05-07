7e8dfcb25e57d2c94320703677f926b9ea05f9a2
p4nthera115 <asdshuja@hotmail.co.uk>
2023-05-07 19:19:32 +0100

    Finished Categories UI

--- Next/airbnb-clone-tut/app/components/CategoryBox.tsx
+++ Next/airbnb-clone-tut/app/components/CategoryBox.tsx
@@ -1,22 +1,53 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps {
   label: string;
   icon: IconType;
   selected?: boolean;
-  description: string;
 }
 
 const CategoryBox: React.FC<CategoryBoxProps> = ({
   label,
   icon: Icon,
   selected,
-  description,
 }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
   return (
     <div
+      onClick={handleClick}
       className={`
         flex
         flex-col
