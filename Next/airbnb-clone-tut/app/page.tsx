5ad3aae12d7e99ad75f3db467b9ba52388057009
p4nthera115 <asdshuja@hotmail.co.uk>
2023-05-13 22:56:26 +0100

    test

--- Next/airbnb-clone-tut/app/page.tsx
+++ Next/airbnb-clone-tut/app/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 import EmptyState from "./components/EmptyState";
 import ClientOnly from "./components/ClientOnly";
